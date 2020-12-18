6c0da28a8c70fe97571dc3c9e46bad8c3605e918
oskarcokl <oskar.cokl@gmail.com>
2020-12-18 16:32:52 +0100

    Split the image dataset into test and train. Also made a python script which does it for me Might convert the script into a proper cl utility.

---
+++ app/bovw_sift/dataset_split.py
@@ -0,0 +1,29 @@
+import numpy as np
+from shutil import copy
+import glob
+from sklearn.model_selection import train_test_split
+
+folder = "..\\..\\data"
+
+
+
+def load_images_from_folder(folder):
+	images = []
+
+	for imagePath in glob.glob(folder + "/*.jpg"):
+		images.append(imagePath)
+
+	images_train, images_test = train_test_split(images, test_size=0.2)
+
+
+	for image in images_train:
+		print("Copying ", image)
+		dst = "../../data/train"
+		copy(image, dst)
+
+	for image in images_test:
+		print("Copying ", image)
+		dst = "../../data/test"
+		copy(image, dst)
+
+load_images_from_folder(folder)
